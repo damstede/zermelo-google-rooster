2ee0a0c332a75ba90aa4dcbe3bc874b5c6b38b39
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2020-03-16 15:43:54 +0100

    versie 1

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
+console.log("Starting...");
+
 const fs = require('fs');
 const readline = require('readline');
+const stc = require('string-to-color');
+const nearestColor = require('nearest-color').from({
+    1: "#7986cb",
+    2: "#33b679",
+    3: "#8e24aa",
+    4: "#e67c73",
+    5: "#f6c026",
+    6: "#f5511d",
+    7: "#039be5",
+    8: "#616161",
+    9: "#3f51b5",
+    10: "#0b8043",
+    11: "#d60000"
+});
 const {google} = require('googleapis');
+const zermeloPrivateAPI = require('./zermelo-private-api.js');
 
 const SCOPES = ['https://www.googleapis.com/auth/calendar'];
 const TOKEN_PATH = 'token.json';
@@ -48,24 +65,170 @@ function getAccessToken(oAuth2Client, callback) {
 
 function updateCalendar(auth) {
     const calendar = google.calendar({version: 'v3', auth});
-    calendar.events.list({
-        calendarId: 'primary',
-        timeMin: (new Date()).toISOString(),
-        maxResults: 10,
-        singleEvents: true,
-        orderBy: 'startTime'
-    }, function(err, res) {
-        if (err) return console.error('The API returned an error: ', err);
-        const events = res.data.items;
-        if (events.length > 0) {
-            console.log("Upcoming 10 events:");
-            events.map(function(event, i) {
-                const start = event.start.dateTime || event.start.date;
-                console.log(`${start} - ${event.summary}`);
-            });
+
+    let zermeloCredentials = JSON.parse(fs.readFileSync("credentials-z.json"));
+
+    zermeloPrivateAPI.setSchool(zermeloCredentials.school);
+    zermeloPrivateAPI.setBranchOfSchool(zermeloCredentials.branch);
+    zermeloPrivateAPI.setApiToken(zermeloCredentials.token);
+
+    zermeloPrivateAPI.getUpcomingAppointments()
+        .then(function(schedule) {
+            let lessonCount = schedule.length;
+            addToCalendar(auth, calendar, schedule, 0, lessonCount);
+            console.log("Done");
+        }).catch(function(err) {
+            console.error(err);
+        });
+}
+
+function addToCalendar(auth, calendar, schedule, i, lessonCount) {
+    console.log("Adding " + i + " to calendar... ("+(lessonCount - i - 1) + " remaining)");
+    // console.log(schedule[i]);
+    if (schedule[i]["teachers"].length > 0 && schedule[i]["students"].length > 0) {
+        calendar.events.get({
+            calendarId: 'damstede.eu_dtdrvnpojua3snqiqlcs2a64fc@group.calendar.google.com',
+            eventId: 'damstederooster'+schedule[i]['appointmentInstance']
+        }, function(err, res) {
+            if (err) {
+                // event does not exist yet, create it
+                let calEvent = {
+                    id: 'damstederooster'+schedule[i]['appointmentInstance'],
+                    summary: schedule[i]["subjects"].join(", ").toUpperCase() + " van " + schedule[i]["teachers"].join(", ").toUpperCase(),
+                    description: "<b>" + schedule[i]["subjects"].join(", ").toUpperCase() + " van " + schedule[i]["teachers"].join(", ").toUpperCase() + " aan " + schedule[i]["groups"].join(", ").toUpperCase() + "</b>" + (schedule[i]["changeDescription"] ? "<br><br>"+schedule[i]["changeDescription"] : ""),
+                    location: schedule[i]["locations"].join(", "),
+                    colorId: parseInt(nearestColor(stc(schedule[i]["subjects"][0]))["name"]),
+                    start: {
+                        dateTime: new Date(schedule[i]["start"] * 1000).toISOString(),
+                        timeZone: "Europe/Amsterdam"
+                    },
+                    end: {
+                        dateTime: new Date(schedule[i]["end"] * 1000).toISOString(),
+                        timeZone: "Europe/Amsterdam"
+                    },
+                    attendees: [
+
+                    ],
+                    guestsCanInviteOthers: false,
+                    guestsCanModify: false,
+                    guestsCanSeeOtherGuests: true,
+                    reminders: {
+                        useDefault: false,
+                        overrides: [
+                            {
+                                method: 'email',
+                                minutes: 10
+                            }
+                        ]
+                    },
+                    source: {
+                        title: "Zermelo Rooster",
+                        url: "https://damstedelyceum.zportal.nl/"
+                    },
+                    status: (schedule[i]["cancelled"] ? "cancelled" : "confirmed"),
+                    conferenceData: {
+                        createRequest: {
+                            requestId: schedule[i]['appointmentInstance']
+                        }
+                    }
+                };
+
+                for (let d = 0; d < schedule[i]["teachers"].length; d++) {
+                    calEvent.attendees.push({
+                        email: schedule[i]["teachers"][d] + "@damstede.eu"
+                    });
+                }
+                for (let d = 0; d < schedule[i]["students"].length; d++) {
+                    calEvent.attendees.push({
+                        email: "n" + schedule[i]["students"][d] + "@damstede.eu"
+                    });
+                }
+
+                calendar.events.insert({
+                    auth: auth,
+                    calendarId: 'damstede.eu_dtdrvnpojua3snqiqlcs2a64fc@group.calendar.google.com',
+                    resource: calEvent
+                }, function(err, newEvent) {
+                    if (err) {
+                        console.error("There was an error contacting the calendar service: ", err);
+                    }
+                    console.log("Lesson created", i);
+                    i++;
+                    if (i < lessonCount) {
+                        console.log("Another round incoming");
+                        setTimeout(function() {
+                            console.log("Running now");
+                            addToCalendar(auth, calendar, schedule, i, lessonCount);
+                        }, 100);
+                    }
+                });
+            }
+            else {
+                // event exists, update it (if needed)
+                console.log("Lesson already exists in the schedule! Updating info (if needed)...", i);
+                if (schedule[i]["teachers"].length > 0 && schedule[i]["students"].length > 0) {
+                    let calEvent = {
+                        status: (schedule[i]["cancelled"] ? "cancelled" : "confirmed"),
+                        summary: schedule[i]["subjects"].join(", ").toUpperCase() + " van " + schedule[i]["teachers"].join(", ").toUpperCase(),
+                        description: "<b>" + schedule[i]["subjects"].join(", ").toUpperCase() + " van " + schedule[i]["teachers"].join(", ").toUpperCase() + " aan " + schedule[i]["groups"].join(", ").toUpperCase() + "</b>" + (schedule[i]["changeDescription"] ? "<br><br>"+schedule[i]["changeDescription"] : ""),
+                        location: schedule[i]["locations"].join(", "),
+                        colorId: parseInt(nearestColor(stc(schedule[i]["subjects"][0]))["name"]),
+                        start: {
+                            dateTime: new Date(schedule[i]["start"] * 1000).toISOString(),
+                            timeZone: "Europe/Amsterdam"
+                        },
+                        end: {
+                            dateTime: new Date(schedule[i]["end"] * 1000).toISOString(),
+                            timeZone: "Europe/Amsterdam"
+                        },
+                        attendees: []
+                    };
+                    for (let d = 0; d < schedule[i]["teachers"].length; d++) {
+                        calEvent.attendees.push({
+                            email: schedule[i]["teachers"][d] + "@damstede.eu"
+                        });
+                    }
+                    for (let d = 0; d < schedule[i]["students"].length; d++) {
+                        calEvent.attendees.push({
+                            email: "n" + schedule[i]["students"][d] + "@damstede.eu"
+                        });
+                    }
+                    calendar.events.patch({
+                        calendarId: 'damstede.eu_dtdrvnpojua3snqiqlcs2a64fc@group.calendar.google.com',
+                        eventId: 'damstederooster'+schedule[i]['appointmentInstance'],
+                        resource: calEvent
+                    });
+                }
+                else {
+                    calendar.events.patch({
+                        calendarId: 'damstede.eu_dtdrvnpojua3snqiqlcs2a64fc@group.calendar.google.com',
+                        eventId: 'damstederooster'+schedule[i]['appointmentInstance'],
+                        resource: {
+                            status: "cancelled"
+                        }
+                    });
+                }
+
+                i++;
+                if (i < lessonCount) {
+                    console.log("Another round incoming");
+                    setTimeout(function() {
+                        console.log("Running now");
+                        addToCalendar(auth, calendar, schedule, i, lessonCount);
+                    }, 100);
+                }
+            }
+        });
+    }
+    else {
+        console.log("Lesson does not contain students or teachers!");
+        i++;
+        if (i < lessonCount) {
+            console.log("Another round incoming");
+            setTimeout(function() {
+                console.log("Running now");
+                addToCalendar(auth, calendar, schedule, i, lessonCount);
+            }, 100);
         }
-        else {
-            console.log("No upcoming events found.");
-        }
-    });
+    }
 }
---
+++ zermelo-private-api.js
@@ -0,0 +1,71 @@
+const https = require('https');
+
+let subdomain = null;
+let accessToken = null;
+let branchOfSchool = null;
+
+exports.setSchool = function(school) {
+    subdomain = school;
+};
+
+exports.setApiToken = function(token) {
+    accessToken = token;
+};
+
+exports.setBranchOfSchool = function(branch) {
+    branchOfSchool = branch;
+};
+
+let getStartTimestamp = function() {
+    return Math.floor((new Date().getTime()) / 1000);
+};
+
+let getEndTimestamp = function() {
+    return Math.floor((new Date().getTime() + 86400000) / 1000);
+};
+
+let getHostName = function() {
+    return subdomain+".zportal.nl";
+};
+
+let getPath = function(endPoint) {
+    return "/api/v3/"+endPoint+"?access_token="+encodeURIComponent(accessToken);
+};
+
+let getApiUrl = function(endPoint) {
+    return "https://"+getHostName()+getPath(endPoint);
+};
+
+exports.getUpcomingAppointments = function() {
+    return new Promise(function(resolve, reject) {
+        let req = https.request({
+            hostname: getHostName(),
+            port: 443,
+            path: getPath("appointments") + "&valid=true&start="+getStartTimestamp()+"&end="+getEndTimestamp()+"&branchOfSchool="+branchOfSchool+"&fields=id,appointmentInstance,start,end,startTimeSlot,endTimeSlot,subjects,teachers,locations,students,groups,lastModified,new,cancelled,teacherChanged,groupChanged,locationChanged,timeChanged,changeDescription",
+            method: 'GET'
+        }, function(res) {
+            let response = "";
+            res.on('data', function(chunk) {
+                response += chunk;
+            });
+            res.on('end', function() {
+                try {
+                    response = JSON.parse(response)["response"];
+                    if (response["status"] == 200) {
+                        resolve(response["data"]);
+                    }
+                    else {
+                        reject(response["status"] + " " + response["message"] + " " + response["details"]);
+                    }
+                }
+                catch(err) {
+                    reject(err);
+                }
+            });
+        })
+        req.on('error', function(err) {
+            reject(err);
+        });
+        req.end();
+    });
+};
